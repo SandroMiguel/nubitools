3e18f3cc0376a5182a796042f32c50ef1bc4c9bf
SandroMiguel <sandromiguel@sandromiguel.com>
2025-05-29 15:29:47 +0100

    feat(promptInput.js): generic prompt for user input with optional custom validation

---
+++ src/cli/promptInput.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+/**
+ * Generic prompt for user input with optional custom validation.
+ *
+ * @param {string} message The message to display.
+ * @param {(input: string) => boolean | string} [validate] Validation function (optional).
+ *
+ * @returns {Promise<string>} User input.
+ */
+export async function promptInput(message, validate) {
+  const { input } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "input",
+      message,
+      validate,
+    },
+  ]);
+
+  return input;
+}
