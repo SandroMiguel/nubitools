e20057f99d648849de98c337578bbb5c877e6820
SandroMiguel <sandromiguel@sandromiguel.com>
2025-06-05 12:52:29 +0100

    chore: ignore __test-project__ from git to avoid large yarn cache

--- __test-project__/index.js
+++
@@ -1,8 +0,0 @@
-import { promptInput } from '../src/cli/promptInput.js'
-
-const main = async () => {
-  const input = await promptInput('Say something:')
-  console.log('You said:', input)
-}
-
-main()
